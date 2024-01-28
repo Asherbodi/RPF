7246dbd24f81d96644fc8f14487a20713734666d
Asherbodi@gmail.com <82899179+Asherbodi@users.noreply.github.com>
2024-01-28 00:58:04 -0500

    UPD

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ const Header = () => {
   return ( <header className='py-8'>
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
-          <a href="https://google.com" onClick={handleClick}>
+          <a href="https://google.com" />
             <img src={ Logo } alt="Logo" className="w-24 h-auto" />
-          </a>
           <button className='btn btn-sm'>Let's connect</button>
         </div>  
       </div> 
